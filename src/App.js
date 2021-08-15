ef55362e08a1a4c96c84222c86e3d48a2d2288a7
Shahzeb Malik <33540322+ShahzebMalik107@users.noreply.github.com>
2021-08-15 20:36:05 +0500

    React Custom wrapper

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from './comonents/ExpenseItem';
+import ExpenseItem from './comonents/ExpenseComponent/NewExpenceComponent';
 
 
 function App() {
@@ -7,28 +7,26 @@ function App() {
       id: 'e1',
       title: 'Expence 1',
       amount: 64.12,
-      date: new Date(20,7,2021)
+      date: new Date(2021, 5 , 20)
     },
     {
       id: 'e2',
       title: 'Expence 2',
       amount: 12.12,
-      date: new Date(20,8,2021)
+      date: new Date(2021,4,15)
     },
     {
       id: 'e3',
       title: 'Expence 3',
       amount: 1532.12,
-      date: new Date(29,7,2021)
+      date: new Date(2021,6,15)
     }
-  ]
+  ];
 
   return (
     <div className="container">
       <center><h2>Expences Creation app</h2></center>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} ></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} ></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} ></ExpenseItem>
+      <ExpenseItem expenses={expenses}/>
     </div>
   );
 }
---
+++ src/comonents/ExpenseComponent/ExpenseDate.js
@@ -0,0 +1,24 @@
+import './ExprenseDate.css'
+
+function ExpenseDate(props) {
+
+    const month = props.date.toLocaleString('en-us', { month: 'long' });
+    const days = props.date.toLocaleString('en-us', { day: '2-digit' });
+    const year = props.date.getFullYear();
+    return (
+        <div className="expense-date">
+            <div className="expense-date__month">
+                {month}
+            </div>
+            <div className="expense-date__year">
+                {year}
+            </div>
+            <div className="expense-date__day">
+                {days}
+            </div>
+        </div>
+    );
+
+}
+
+export default ExpenseDate;
